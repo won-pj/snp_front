684c2cd2faaff076b996956d158adabb6e1e20b0
hyogeun <yhdok1110@naver.com>
2023-10-15 18:34:41 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [text, setText] = useState("없음");
+  
+  const clicked = () => {
+    axios
+      .get("http://127.0.0.1:8000", {
+        params: {
+          abc: "가나다",
+        },
+      })
+      .then((response) => setText(JSON.stringify(response.data)));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{text}</h1>
+      <button onClick={clicked}>클릭</button>
     </div>
   );
 }
